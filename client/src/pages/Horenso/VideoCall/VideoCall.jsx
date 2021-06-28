c150dcb38d43097427c905621402b77a0dae8cc9
pum-k <ddkhoa1206@gmail.com>
2021-06-28 22:44:51 +0700

    create folder assets & move folder images to it

--- client/src/pages/Horenso/VideoCall/VideoCall.jsx
+++ client/src/pages/Horenso/VideoCall/VideoCall.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import Chatbox from "./components/Chatbox/Chatbox";
-import ChatField from "../../../features/ChatField/ChatField";
+
 const VideoCall = () => {
-  return (
-    <div>
-      <p>Hello world</p>
-      <p>Hello</p>
-      <Chatbox />
-      <ChatField />
-    </div>
-  );
+  return <div>
+    
+  </div>;
 };
 
 export default VideoCall;
