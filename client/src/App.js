2dff904fe3d4850ea3cdd74ff73ed42ca989df95
pum-k <ddkhoa1206@gmail.com>
2021-06-30 12:11:46 +0700

    commit to setting push

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ import So from "./pages/Horenso/So/So";
 function App() {
   return (
     <div className="App">
-      {/* <SideBar />
-      <So /> */}
+      <SideBar />
+      <So />
     </div>
   );
 }
