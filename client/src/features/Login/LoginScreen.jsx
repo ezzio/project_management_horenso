9e4895f314693b97e0515ad556e5235a394452dc
mrminhb <novafalcongamers@gmail.com>
2021-06-30 22:00:24 +0700

    UI component cho Login

---
+++ client/src/features/Login/LoginScreen.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./LoginScreen.scss"
+import MaterialCommunityIconsIcon from "react-native-vector-icons/dist/MaterialCommunityIcons";
+
+
+function LoginScreen() {
+    return (
+        <div className='login'>
+            <div className='login__form'>
+                <LoginForm></LoginForm>
+            </div>
+            <button className='help-btn' 
+                    style={{
+                        height: 60,
+                        width: 60,
+                        marginLeft: 13,
+                        marginTop: 325  
+                    }}>
+                <MaterialCommunityIconsIcon
+                    name="comment-question"
+                    style={{
+                        color: "#fff",
+                        fontSize: 24
+                    }}
+                ></MaterialCommunityIconsIcon>
+                <span className='help-btn__text'>Help</span>
+            </button>
+            <div className='login__signup'>
+                <span className='long__signup__text'>Don't have an account?</span>
+            </div>
+            <button className='signup-btn' style={{
+                    height: 36,
+                    width: 100
+                     }}>
+                <span className='ignup-btn__text'>Sign up</span>                
+            </button>
+            <div className='login__slogan-row'>
+                <div className='login__slogan-row__stack'>
+                    <input className='login__slogan-row__stack__text'>This is the slogan</input>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginScreen
---
+++ client/src/features/Login/component/LoginForm.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import './LoginForm.scss'
+import EntypoIcon from "react-native-vector-icons/dist/Entypo";
+
+function LoginForm() {
+    return (
+        <div className='form'>
+            <div className='form__title'>
+                <span className='form__title__text'>Welcome!</span>
+            </div>
+            <div className='form__email-container'>
+                <div className='email-textbox' 
+                    style={{
+                        height: 45,
+                        width: 400,
+                        position: "absolute",
+                        left: 0,
+                        top: 23,
+                        borderWidth: 1,
+                        borderColor: "#000000",
+                        borderStyle: "solid"
+                }}>
+                    <input className='email-textbox__text' placeholder='Enter your email'></input>
+                    <input className='email-textbox__input'></input>
+                </div>
+                <span className='form__email-container__label'>Email</span>
+            </div>
+            <div className='form__password-container'>
+                <div className='password-textbox' 
+                    style={{
+                        height: 45,
+                        width: 400,
+                        position: "absolute",
+                        left: 0,
+                        top: 0,
+                        borderWidth: 1,
+                        borderColor: "#000000",
+                        borderStyle: "solid"
+                    }}>
+                        <input className='password-textbox__text' placeholder='Enter password'></input>
+                        <input className='password-textbox__input'></input>
+                </div>
+                <span className='form__password-container__label'>Password</span>
+            </div>
+            <EntypoIcon
+                name="mail"
+                style={{
+                top: 129,
+                left: 23,
+                position: "absolute",
+                color: "rgba(128,128,128,1)",
+                fontSize: 40
+                }}
+            ></EntypoIcon>
+            <EntypoIcon
+                name="lock"
+                style={{
+                top: 213,
+                left: 23,
+                position: "absolute",
+                color: "rgba(128,128,128,1)",
+                fontSize: 40
+            }}
+            ></EntypoIcon>
+            <span className='form__login-github'>Or login with Github</span>
+        </div>
+    )
+}
+
+export default LoginForm
