69e48b9c022bf142833614bc7c599e779c262539
lcnhut <lcnhut070200@gmail.com>
2021-07-11 21:38:40 +0700

    fix kich thuoc container projectchecking

--- client/src/pages/Kanban/ProjectChecking/components/TaskColumn/TaskColumn.jsx
+++ client/src/pages/Kanban/ProjectChecking/components/TaskColumn/TaskColumn.jsx
@@ -7,18 +7,109 @@ function TaskColumn() {
                 <p className="task-infomation">To Do</p>
                 <div className="task-detail">
                     <div className="task-header">
-                        <p className="task-name">Design homepage</p>
+                        <p className="task-header-name">Design homepage</p>
                         <span>
-                            <a href="" className="more">...</a>
+                            <a href="" className="task-header-more">...</a>
                         </span>
                     </div>
                     <div className="task-content">
-                        <p className="task-member">A</p>
-                        <p className="task-time">00:00:00 h</p>
+                        <p className="task-content-member">A</p>
+                        <p className="task-content-time">00:00:00 h</p>
+                    </div>
+                </div>
+
+                <div className="task-detail">
+                    <div className="task-header">
+                        <p className="task-header-name">Design homepage</p>
+                        <span>
+                            <a href="" className="task-header-more">...</a>
+                        </span>
+                    </div>
+                    <div className="task-content">
+                        <p className="task-content-member">A</p>
+                        <p className="task-content-time">00:00:00 h</p>
+                    </div>
+                </div>
+
+                <div className="task-detail">
+                    <div className="task-header">
+                        <p className="task-header-name">Design homepage</p>
+                        <span>
+                            <a href="" className="task-header-more">...</a>
+                        </span>
+                    </div>
+                    <div className="task-content">
+                        <p className="task-content-member">A</p>
+                        <p className="task-content-time">00:00:00 h</p>
+                    </div>
+                </div>
+
+                <div className="task-detail">
+                    <div className="task-header">
+                        <p className="task-header-name">Design homepage</p>
+                        <span>
+                            <a href="" className="task-header-more">...</a>
+                        </span>
+                    </div>
+                    <div className="task-content">
+                        <p className="task-content-member">A</p>
+                        <p className="task-content-time">00:00:00 h</p>
+                    </div>
+                </div>
+
+                <div className="task-detail">
+                    <div className="task-header">
+                        <p className="task-header-name">Design homepage</p>
+                        <span>
+                            <a href="" className="task-header-more">...</a>
+                        </span>
+                    </div>
+                    <div className="task-content">
+                        <p className="task-content-member">A</p>
+                        <p className="task-content-time">00:00:00 h</p>
+                    </div>
+                </div>
+
+                <div className="task-detail">
+                    <div className="task-header">
+                        <p className="task-header-name">Design homepage</p>
+                        <span>
+                            <a href="" className="task-header-more">...</a>
+                        </span>
+                    </div>
+                    <div className="task-content">
+                        <p className="task-content-member">A</p>
+                        <p className="task-content-time">00:00:00 h</p>
+                    </div>
+                </div>
+
+                <div className="task-detail">
+                    <div className="task-header">
+                        <p className="task-header-name">Design homepage</p>
+                        <span>
+                            <a href="" className="task-header-more">...</a>
+                        </span>
+                    </div>
+                    <div className="task-content">
+                        <p className="task-content-member">A</p>
+                        <p className="task-content-time">00:00:00 h</p>
+                    </div>
+                </div>
+
+                <div className="task-detail">
+                    <div className="task-header">
+                        <p className="task-header-name">Design homepage</p>
+                        <span>
+                            <a href="" className="task-header-more">...</a>
+                        </span>
+                    </div>
+                    <div className="task-content">
+                        <p className="task-content-member">A</p>
+                        <p className="task-content-time">00:00:00 h</p>
                     </div>
                 </div>
                 <div className="new-task">
-                    <a href="">+Add new task</a>
+                    <a href="" className="">+Add new task</a>
                 </div>
             </div>
         </div>
