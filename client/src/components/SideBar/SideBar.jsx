3403232d087076db710f1dca17084666ce0d5cb8
pum-k <ddkhoa1206@gmail.com>
2021-08-02 00:56:17 +0700

    feat: create focus for sidebar selector

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,34 +1,42 @@
-import React, { useRef, useState } from "react";
-import "./SideBar.scss";
-import { Link } from "react-router-dom";
-import { SiGooglehangoutsmeet } from "react-icons/si";
+import React, { useRef, useState } from 'react';
+import './SideBar.scss';
+import { Link, useLocation } from 'react-router-dom';
+import { SiGooglehangoutsmeet } from 'react-icons/si';
 import {
   BsFillKanbanFill,
   BsFillChatDotsFill,
   BsThreeDots,
-} from "react-icons/bs";
+} from 'react-icons/bs';
 import {
   AiFillGithub,
   AiFillSetting,
   AiFillSignal,
   AiFillDatabase,
-} from "react-icons/ai";
-import { GiFlowerTwirl } from "react-icons/gi";
-import { RiTeamFill } from "react-icons/ri";
-import { MdDashboard } from "react-icons/md";
+} from 'react-icons/ai';
+import { GiFlowerTwirl } from 'react-icons/gi';
+import { RiTeamFill } from 'react-icons/ri';
+import { MdDashboard } from 'react-icons/md';
 const SideBar = () => {
   const mouseEnter = () => {
-    let hiddenElement = document.querySelectorAll(".hidden");
-    for (var i = 0, len = hiddenElement.length; i < len; i++) {
-      hiddenElement[i].style.display = "block";
-    }
+    setTimeout(() => {
+      let hiddenElement = document.querySelectorAll('.hidden');
+      for (var i = 0, len = hiddenElement.length; i < len; i++) {
+        hiddenElement[i].style.display = 'block';
+      }
+    }, 100);
   };
   const mouseLeave = () => {
-    let hiddenElement = document.querySelectorAll(".hidden");
-    for (var i = 0, len = hiddenElement.length; i < len; i++) {
-      hiddenElement[i].style.display = "none";
-    }
+    setTimeout(() => {
+      let hiddenElement = document.querySelectorAll('.hidden');
+      for (var i = 0, len = hiddenElement.length; i < len; i++) {
+        hiddenElement[i].style.display = 'none';
+      }
+    }, 100);
   };
+
+  const location = useLocation();
+  const currentUrl = location.pathname.slice(1);
+
   return (
     <div
       className="side-bar ctn-side-bar"
@@ -41,46 +49,122 @@ const SideBar = () => {
       </div>
       <div className="side-bar__content">
         <div className="side-bar__content__group">
-          <Link to="/dashboard">
-            <MdDashboard className="icon" /> <p className="hidden">Dashboard</p>
-          </Link>
-          <Link to="/kanban">
-            <BsFillKanbanFill className="icon" />{" "}
-            <p className="hidden">Table</p>
-          </Link>
-          <Link to="/source">
-            <AiFillDatabase className="icon" /> <p className="hidden">Stores</p>
-          </Link>
-          <Link to="/github">
-            <AiFillGithub className="icon" /> <p className="hidden">Github</p>
-          </Link>
+          {currentUrl === 'dashboard' ? (
+            <Link to="/dashboard" className="sidebar-active">
+              <MdDashboard className="icon" />{' '}
+              <p className="hidden">Dashboard</p>
+            </Link>
+          ) : (
+            <Link to="/dashboard">
+              <MdDashboard className="icon" />{' '}
+              <p className="hidden">Dashboard</p>
+            </Link>
+          )}
+          {currentUrl === 'kanban' ? (
+            <Link className="sidebar-active" to="/kanban">
+              <BsFillKanbanFill className="icon" />{' '}
+              <p className="hidden">Table</p>
+            </Link>
+          ) : (
+            <Link to="/kanban">
+              <BsFillKanbanFill className="icon" />{' '}
+              <p className="hidden">Table</p>
+            </Link>
+          )}
+
+          {currentUrl === 'source' ? (
+            <Link className="sidebar-active" to="/source">
+              <AiFillDatabase className="icon" />{' '}
+              <p className="hidden">Stores</p>
+            </Link>
+          ) : (
+            <Link to="/source">
+              <AiFillDatabase className="icon" />{' '}
+              <p className="hidden">Stores</p>
+            </Link>
+          )}
+
+          {currentUrl === 'github' ? (
+            <Link className="sidebar-active" to="/github">
+              <AiFillGithub className="icon" /> <p className="hidden">Github</p>
+            </Link>
+          ) : (
+            <Link to="/github">
+              <AiFillGithub className="icon" /> <p className="hidden">Github</p>
+            </Link>
+          )}
         </div>
         <div className="side-bar__content__group">
-          <Link to="/meeting">
-            <SiGooglehangoutsmeet className="icon" />{" "}
-            <p className="hidden">Meeting</p>
-            <div className="notify hidden">
-              <p>2</p>
-            </div>
-          </Link>
-          <Link to="/conversation">
-            <BsFillChatDotsFill className="icon" />{" "}
-            <p className="hidden">Conversation</p>
-            <div className="notify hidden">
-              <p>9+</p>
-            </div>
-          </Link>
-          <Link to="/kanban">
-            <AiFillSignal className="icon" /> <p className="hidden">Reports</p>
-          </Link>
+          {currentUrl === 'meeting' ? (
+            <Link className="sidebar-active" to="/meeting">
+              <SiGooglehangoutsmeet className="icon" />{' '}
+              <p className="hidden">Meeting</p>
+              <div className="notify hidden">
+                <p>2</p>
+              </div>
+            </Link>
+          ) : (
+            <Link to="/meeting">
+              <SiGooglehangoutsmeet className="icon" />{' '}
+              <p className="hidden">Meeting</p>
+              <div className="notify hidden">
+                <p>2</p>
+              </div>
+            </Link>
+          )}
+
+          {currentUrl === 'conversation' ? (
+            <Link className="sidebar-active" to="/conversation">
+              <BsFillChatDotsFill className="icon" />{' '}
+              <p className="hidden">Conversation</p>
+              <div className="notify hidden">
+                <p>9+</p>
+              </div>
+            </Link>
+          ) : (
+            <Link to="/conversation">
+              <BsFillChatDotsFill className="icon" />{' '}
+              <p className="hidden">Conversation</p>
+              <div className="notify hidden">
+                <p>9+</p>
+              </div>
+            </Link>
+          )}
+
+          {currentUrl === 'report' ? (
+            <Link className="sidebar-active" to="/report">
+              <AiFillSignal className="icon" />{' '}
+              <p className="hidden">Reports</p>
+            </Link>
+          ) : (
+            <Link to="/report">
+              <AiFillSignal className="icon" />{' '}
+              <p className="hidden">Reports</p>
+            </Link>
+          )}
         </div>
         <div className="side-bar__content__group">
-          <Link to="/team-mate">
-            <RiTeamFill className="icon" /> <p className="hidden">Teammate</p>
-          </Link>
-          <Link to="/setting">
-            <AiFillSetting className="icon" /> <p className="hidden">Setting</p>
-          </Link>
+          {currentUrl === 'team-mate' ? (
+            <Link className="sidebar-active" to="/team-mate">
+              <RiTeamFill className="icon" /> <p className="hidden">Teammate</p>
+            </Link>
+          ) : (
+            <Link to="/team-mate">
+              <RiTeamFill className="icon" /> <p className="hidden">Teammate</p>
+            </Link>
+          )}
+
+          {currentUrl === 'setting' ? (
+            <Link className="sidebar-active" to="/setting">
+              <AiFillSetting className="icon" />{' '}
+              <p className="hidden">Setting</p>
+            </Link>
+          ) : (
+            <Link to="/setting">
+              <AiFillSetting className="icon" />{' '}
+              <p className="hidden">Setting</p>
+            </Link>
+          )}
         </div>
       </div>
       <div className="side-bar__footer">
