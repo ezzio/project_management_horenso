9b08c1d2c6f3c30f837572e2f3655efa12112ab1
lcnhut <lcnhut070200@gmail.com>
2021-08-03 16:04:44 +0700

    fix data statistics

--- client/src/pages/Kanban/ProjectChecking/components/Statistics/Statistics.jsx
+++ client/src/pages/Kanban/ProjectChecking/components/Statistics/Statistics.jsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Statistics.scss";
 import {BsBag} from 'react-icons/bs';
 import {GrMoney, GrPlan} from 'react-icons/gr';
 import {GiProgression} from 'react-icons/gi';
 
 function Statistics(props) {
-    const data = [
-        {name: 'Total time on Projects', value: '03:39', unit: 'h', icon: <BsBag/>},
-        {name: 'Earnings', value: '2,409.20', unit: '$', icon: <GrMoney/>},
-        {name: 'Costs', value: '1,260.14', unit: '$', icon: <GrPlan/>},
-        {name: 'Productivity', value: '93.57', unit: '%', icon: <GiProgression/>},
-]
+    const [time, setTime] = useState({
+        value: '03:39',
+    });
+
+    const [earning, setEarning] = useState({
+        value: '2,409.20',
+    });
+
+    const [cost, setCost] = useState({
+        value: '1,260.14',
+    });
+
+    const [productivity, setProductivity] = useState({
+        value: '93.57',
+    });
     return (
         <div className="statistics">
             <div className="parameter-list">
-                {data.map((d) => (
-                    <div className="parameter-item">
-                    <p className="parameter-name">{d.name}</p>
-                    <h2 className="parameter-value">{d.value} {d.unit}</h2>
-                    <i className='parameter-icon'>{d.icon}</i>
+                <div className="parameter-item">
+                    <p className="parameter-name">Total time on Projects</p>
+                    <h2 className="parameter-value">{time.value} h</h2>
+                    <i className='parameter-icon'><BsBag/></i>
                 </div>
-                ))}
+                <div className="parameter-item">
+                    <p className="parameter-name">Earnings</p>
+                    <h2 className="parameter-value">${earning.value}</h2>
+                    <i className='parameter-icon'><GrMoney/></i>
+                </div>
+                <div className="parameter-item">
+                    <p className="parameter-name">Costs</p>
+                    <h2 className="parameter-value">${cost.value}</h2>
+                    <i className='parameter-icon'><GrPlan/></i>
+                </div>
+                <div className="parameter-item">
+                    <p className="parameter-name">Productivity</p>
+                    <h2 className="parameter-value">{productivity.value} %</h2>
+                    <i className='parameter-icon'><GiProgression/></i>
+                </div>
             </div>
         </div>
     );
