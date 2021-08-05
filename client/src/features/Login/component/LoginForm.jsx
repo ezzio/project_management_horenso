5f81a3e2f9b389a87fbebdb12ba39ee1e022af11
mrminhb <novafalcongamers@gmail.com>
2021-08-05 19:55:34 +0700

    feat [login]: add login function react-hook-form

--- client/src/features/Login/component/LoginForm.jsx
+++ client/src/features/Login/component/LoginForm.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react'
 import './LoginForm.scss'
+import { useForm } from 'react-hook-form'
 import { AiOutlineMail, AiOutlineLock } from 'react-icons/ai';
 import { Link } from 'react-router-dom'
+
 
 function LoginForm(props) {
     const {Login, error} = props
 
-    const [details, setDetails] = useState({email: '', password: ''})
+    const {register, reset, handleSubmit, formState: { errors } } = useForm()
 
-    const submitHandler = (e) => {
-        e.preventDefault()
-        Login(details)
-        setDetails({email: '', password: ''})
+    const submitHandler = (data) => {
+        Login(data)
+        reset({
+            email: data.email,
+            password: ''
+        })
     }
 
     return (
-        <div className='login-form'>
+        <form onSubmit={handleSubmit(submitHandler)} className='login-form'>
             <div className='login-form__title'>
                 <span className='login-form__title__text'>Welcome!</span>
             </div>
@@ -32,11 +36,13 @@ function LoginForm(props) {
                         borderStyle: "solid"
                 }}>
                     <input className='email-textbox__input' placeholder='Enter your email' 
-                    type='text' name='email' id='email' value={details.email}
-                    onChange={(e) => setDetails({...details, name: e.target.value})}
+                    type='text' name='email' 
+                    {...register("email", { required: "This field is required", 
+                    pattern: {value: /^\S+@\S+$/i, message: 'Invalid email address'} })} 
                     />
+                    <span className='login-form__email-container__label'>Email</span>
                 </div>
-                <span className='login-form__email-container__label'>Email</span>
+                { errors.email && <p style={{ marginTop: 70 }} className='login-form__error'>{errors.email.message}</p> }
             </div>
             <div className='login-form__password-container'>
                 <span className='login-form__password-container__label'>Password</span>
@@ -52,10 +58,13 @@ function LoginForm(props) {
                         borderStyle: "solid"
                 }}>
                     <input className='password-textbox__input' placeholder='Enter password' 
-                    type='password' name='password' id='password' value={details.password}
-                    onChange={(e) => setDetails({...details, name: e.target.value})}
+                    type='password' name='password' 
+                    {...register("password", { required: "This field is required", 
+                    minLength: {value: 6, message: "Password must be 6-18 characters long"}, 
+                    maxLength: {value: 18, message: "Password must be 6-18 characters long"} })} 
                     />
                 </div>
+                { errors.password && <p style={{ marginTop: 50 }} className='login-form__error'>{errors.password.message}</p> }
             </div>
             <Link className='login-form__forgotpassword' path='/ForgotPassword'>Forgot password?</Link>
             <AiOutlineMail style={{ 
@@ -73,15 +82,15 @@ function LoginForm(props) {
                 color: 'gray'
             }} />
             <div>
-                {(error != '') ? (<p style={{color: 'red'}}>{error}</p>) : ''}
+                {(error != '') ? (<p style={{ marginTop: -220, marginLeft: 167 }} className='login-form__error'>{error}</p>) : ''}
             </div>
             <div className='login-form__login-btn-container'>
-                <button className='login-btn'>
+                <button type='submit' className='login-btn'>
                     <span className='login-btn__text'>Log In</span>
                 </button>
             </div>
             <a className='login-form__login-github' href='https://www.google.com'>Or login with Github</a>
-        </div>
+        </form>
     )
 }
 
