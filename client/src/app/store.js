107bb4cdb42588c6bf2f0219991ed93144450df0
pum-k <ddkhoa1206@gmail.com>
2021-08-30 10:11:34 -0400

    chore: initial build board kanban

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD
-=======
-import loginReducer from "features/Login/LoginSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import loginReducer from 'features/Login/LoginSlice';
 
->>>>>>> 35491bb605f7d359c035b4c927d509d16553fe0a
 export const store = configureStore({
   reducer: {
     login: loginReducer,
