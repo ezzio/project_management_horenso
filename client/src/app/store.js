a0dc6348712ed818238b961130d89189ef794a31
DESKTOP-KJ9RR1A\ddkho <ddkhoa1206@gmail.com>
2021-09-13 17:22:08 +0700

    fix: resolving conflicts between pum-k and main

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from 'features/Login/LoginSlice';
 import signupReducer from 'features/Signup/SignupSlice';
-import boardReducer from 'features/Board/boardSlice';
 export const store = configureStore({
   reducer: {
     board: boardReducer,
