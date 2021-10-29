fbc6655d61f082f59cede82401d4d941c1380a46
Nguyên Đặng <nguyendang.ct12@gmail.com>
2021-10-29 20:05:48 +0700

    feat: features of KanbanDashBoard

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import loginReducer from 'features/Login/LoginSlice';
-import signupReducer from 'features/Signup/SignupSlice';
-import boardReducer from 'features/Board/boardSlice';
-import timelineReducer from 'features/ReportTimeline/TimelineSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from "features/Login/LoginSlice";
+import signupReducer from "features/Signup/SignupSlice";
+import boardReducer from "features/Board/boardSlice";
+import timelineReducer from "features/ReportTimeline/TimelineSlice";
+import kanbanReducer from "features/KanbanDashBoard/KanbanDashBoardSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     login: loginReducer,
     signup: signupReducer,
     timeline: timelineReducer,
+    kanban: kanbanReducer,
   },
 });
