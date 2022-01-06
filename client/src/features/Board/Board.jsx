15579f416354b0a27b40b72abd3f805636798765
pum-k <ddkhoa1206@gmail.com>
2022-01-06 23:14:22 +0700

    feat: add loading place

--- client/src/features/Board/Board.jsx
+++ client/src/features/Board/Board.jsx
@@ -12,6 +12,7 @@ import {
   fetchBoard,
   updateOnDnd,
 } from './boardSlice';
+import { Spin } from 'antd';
 
 const Board = (props) => {
   const { idBoard } = useParams();
@@ -43,33 +44,44 @@ const Board = (props) => {
   // <----------------------
 
   const members = useSelector((state) => state.board.memberInJob);
+  const loading = useSelector((state) => state.board.loading);
 
   return (
-    <div className="ctn ctn-board">
-      <HeaderBoard
-        members={members}
-        title="Design UI Home Page"
-        status="Active"
-        level="Medium"
-        startTime="10/08/2021"
-        endTime="21/12/2022"
-      />
-      <div>
-        <ModalNewTask
-          jobowner={idBoard}
-          modalOpen={modalOpen}
-          closeModal={closeModal}
+    <Spin
+      tip="Loading..."
+      size="large"
+      spinning={loading}
+      style={{ width: '100%', height: '100%' }}
+    >
+      <div
+        className="ctn ctn-board"
+        style={{ width: '100vw', height: '100vh' }}
+      >
+        <HeaderBoard
           members={members}
+          title="Design UI Home Page"
+          status="Active"
+          level="Medium"
+          startTime="10/08/2021"
+          endTime="21/12/2022"
         />
+        <div>
+          <ModalNewTask
+            jobowner={idBoard}
+            modalOpen={modalOpen}
+            closeModal={closeModal}
+            members={members}
+          />
+        </div>
+        {/* render column */}
+        <div className="board-content">
+          {columns &&
+            columns.map((column) => {
+              return <Column column={column} openModal={openModal} />;
+            })}
+        </div>
       </div>
-      {/* render column */}
-      <div className="board-content">
-        {columns &&
-          columns.map((column) => {
-            return <Column column={column} openModal={openModal} />;
-          })}
-      </div>
-    </div>
+    </Spin>
   );
 };
 
