a1e5ce9c99cc3414b216824e204174288a727c41
pum-k <ddkhoa1206@gmail.com>
2022-01-07 11:55:50 +0700

    fix: remove duplicate import

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,32 @@
-import "antd/dist/antd.css";
-import NotFound from "components/Common/NotFound/NotFound";
-import PrivateRoute from "components/Common/PrivateRoute/PrivateRoute";
-import SideBar from "components/SideBar/SideBar";
-import Board from "features/Board/Board";
-import DetailTask from "features/DetailTask/DetailTask";
-import KanbanDashBoard from "features/KanbanDashBoard/KanbanDashBoard";
-import LoginScreen from "features/Login/LoginScreen";
-import SignupScreen from "features/Signup/SignupScreen";
-import Dashboard from "pages/Dashboard/Dashboard";
-import Github from "pages/Github/Github";
-import Conversation from "pages/Horenso/Conversation/Conversation";
-import Meeting from "pages/Horenso/Meeting/Meeting";
-import Report from "pages/Horenso/Report/Report";
-import Setting from "pages/Setting/Setting";
-import Source from "pages/Storage/Source/Source";
-import Teammate from "pages/Teammate/Teammate";
-import UserSetting from "pages/UserSettings/UserSetting";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+// import components
+import NotFound from 'components/Common/NotFound/NotFound';
+import PrivateRoute from 'components/Common/PrivateRoute/PrivateRoute';
+import SideBar from 'components/SideBar/SideBar';
+import Board from 'features/Board/Board';
+import DetailTask from 'features/DetailTask/DetailTask';
+import KanbanDashBoard from 'features/KanbanDashBoard/KanbanDashBoard';
+import LoginScreen from 'features/Login/LoginScreen';
+import SignupScreen from 'features/Signup/SignupScreen';
+import Dashboard from 'pages/Dashboard/Dashboard';
+import Github from 'pages/Github/Github';
+import Conversation from 'pages/Horenso/Conversation/Conversation';
+import Meeting from 'pages/Horenso/Meeting/Meeting';
+import Report from 'pages/Horenso/Report/Report';
+import Setting from 'pages/Setting/Setting';
+import Source from 'pages/Storage/Source/Source';
+import Teammate from 'pages/Teammate/Teammate';
+import UserSetting from 'pages/UserSettings/UserSetting';
+// import library
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   useLocation,
-} from "react-router-dom";
-import "./App.scss";
 } from 'react-router-dom';
-import Setting from 'pages/Setting/Setting';
+// import css
+import 'antd/dist/antd.css';
+import './App.scss';
 
 function App() {
   return (
@@ -46,7 +47,11 @@ function App() {
             <AnimatedRouter />
           </PrivateRoute>
 
-          <PrivateRoute exact path="/:idProject/:idBoard" children={<Board />} />
+          <PrivateRoute
+            exact
+            path="/:idProject/:idBoard"
+            children={<Board />}
+          />
           <PrivateRoute
             exact
             path="/:idProject/:idBoard/:idTask"
@@ -63,18 +68,18 @@ function App() {
 // Effect change page
 const AnimatedRouter = () => {
   const location = useLocation();
-  const [transitionStage, setTransitionStage] = useState("in");
+  const [transitionStage, setTransitionStage] = useState('in');
   const [displayLocation, setDisplayLocation] = useState(location);
   useEffect(() => {
     if (location.pathname !== displayLocation.pathname)
-      setTransitionStage("out");
+      setTransitionStage('out');
   }, [displayLocation.pathname, location]);
   return (
     <div
-      className={transitionStage === "in" ? "slide-bottom" : "slide-top"}
+      className={transitionStage === 'in' ? 'slide-bottom' : 'slide-top'}
       onAnimationEnd={() => {
-        if (transitionStage === "out") {
-          setTransitionStage("in");
+        if (transitionStage === 'out') {
+          setTransitionStage('in');
           setDisplayLocation(location);
         }
       }}
