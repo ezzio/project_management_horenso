ed3d8f865cbd33f9b21fcd23bff476b328cfce02
pum-k <ddkhoa1206@gmail.com>
2022-01-12 09:48:52 +0700

    feat: fetch, create detail task

--- client/src/features/DetailTask/DetailTask.jsx
+++ client/src/features/DetailTask/DetailTask.jsx
@@ -17,6 +17,8 @@ import {
   Progress,
   Menu,
   Dropdown,
+  Spin,
+  Empty,
 } from 'antd';
 
 import {
@@ -47,6 +49,7 @@ const DetailTask = (props) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const data = useSelector((state) => state.detailTask.allDetailTask);
+  const loading = useSelector((state) => state.detailTask.loading);
 
   const [selectedRowKeys, setSelectedRowKeys] = useState(
     data.filter((item) => item.isCompleted).map((item) => item.key)
@@ -262,147 +265,154 @@ const DetailTask = (props) => {
         </Form>
       </Modal>
 
-      <div
-        style={{
-          width: '100%',
-          minHeight: '100vh',
-          display: 'flex',
-          justifyContent: 'space-between',
-          padding: '1rem',
-          backgroundColor: '#F3F5F7',
-        }}
-      >
-        <Space direction="vertical" style={{ width: '75%' }} size="large">
-          <Space
-            direction="vertical"
-            style={{
-              width: '100%',
-              backgroundColor: 'white',
-              padding: '1rem 2rem',
-              height: '400px',
-              overflow: 'auto',
-              borderRadius: '8px',
-            }}
-          >
-            <PageHeader
-              ghost={false}
-              onBack={() => window.history.back()}
-              title="This is the title of task"
-              style={{ padding: '0px' }}
-              subTitle={<div className="high">High</div>}
-            >
-              <Descriptions size="small" column={6}>
-                <Descriptions.Item>
-                  <Space>
-                    <CalendarOutlined />
-                    <Text>
-                      28/10/2021 <ArrowRightOutlined /> 30/10/2021
-                    </Text>
-                  </Space>
-                </Descriptions.Item>
-              </Descriptions>
-            </PageHeader>
-            <Avatar.Group
-              size="default"
-              maxCount={5}
-              maxStyle={{
-                color: '#f56a00',
-                backgroundColor: '#fde3cf',
+      <Spin tip="Loading..." spinning={loading}>
+        <div
+          style={{
+            width: '100%',
+            minHeight: '100vh',
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: '1rem',
+            backgroundColor: '#F3F5F7',
+          }}
+        >
+          <Space direction="vertical" style={{ width: '75%' }} size="large">
+            <Space
+              direction="vertical"
+              style={{
+                width: '100%',
+                backgroundColor: 'white',
+                padding: '1rem 2rem',
+                height: '400px',
+                overflow: 'auto',
+                borderRadius: '8px',
               }}
             >
-              <Avatar src="https://joeschmoe.io/api/v1/random" />
-              <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
-              <Tooltip title="Ant User" placement="top">
+              <PageHeader
+                ghost={false}
+                onBack={() => window.history.back()}
+                title="This is the title of task"
+                style={{ padding: '0px' }}
+                subTitle={<div className="high">High</div>}
+              >
+                <Descriptions size="small" column={6}>
+                  <Descriptions.Item>
+                    <Space>
+                      <CalendarOutlined />
+                      <Text>
+                        28/10/2021 <ArrowRightOutlined /> 30/10/2021
+                      </Text>
+                    </Space>
+                  </Descriptions.Item>
+                </Descriptions>
+              </PageHeader>
+              <Avatar.Group
+                size="default"
+                maxCount={5}
+                maxStyle={{
+                  color: '#f56a00',
+                  backgroundColor: '#fde3cf',
+                }}
+              >
+                <Avatar src="https://joeschmoe.io/api/v1/random" />
+                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                <Tooltip title="Ant User" placement="top">
+                  <Avatar
+                    style={{ backgroundColor: '#87d068' }}
+                    icon={<UserOutlined />}
+                  />
+                </Tooltip>
                 <Avatar
-                  style={{ backgroundColor: '#87d068' }}
-                  icon={<UserOutlined />}
+                  style={{ backgroundColor: '#1890ff' }}
+                  icon={<AntDesignOutlined />}
                 />
-              </Tooltip>
-              <Avatar
-                style={{ backgroundColor: '#1890ff' }}
-                icon={<AntDesignOutlined />}
-              />
-            </Avatar.Group>
-            <Space direction="vertical">
-              <Title level={5}>Decription:</Title>
-              <Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores perferendis quia ullam quaerat ipsam! Illo
-                consequatur est modi quasi, id quae in quia animi veritatis
-                voluptas dolorem vel, quam nostrum. Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Id dolore quod enim sit veritatis
-                debitis ipsa, iste neque reiciendis, nostrum mollitia
-                doloremque! Fugit animi odio eligendi quae soluta, delectus
-                facere?
-              </Text>
+              </Avatar.Group>
+              <Space direction="vertical">
+                <Title level={5}>Decription:</Title>
+                <Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Asperiores perferendis quia ullam quaerat ipsam! Illo
+                  consequatur est modi quasi, id quae in quia animi veritatis
+                  voluptas dolorem vel, quam nostrum. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Id dolore quod enim sit
+                  veritatis debitis ipsa, iste neque reiciendis, nostrum
+                  mollitia doloremque! Fugit animi odio eligendi quae soluta,
+                  delectus facere?
+                </Text>
+              </Space>
+              <Space direction="vertical" style={{ width: '100%' }}>
+                <Title level={5}>Progress:</Title>
+                <Progress
+                  percent={parseInt(
+                    (selectedRowKeys.length * 100) / data.length
+                  )}
+                  status="active"
+                />
+              </Space>
             </Space>
-            <Space direction="vertical" style={{ width: '100%' }}>
-              <Title level={5}>Progress:</Title>
-              <Progress
-                percent={parseInt((selectedRowKeys.length * 100) / data.length)}
-                status="active"
+            <div
+              style={{
+                width: '100%',
+                backgroundColor: 'white',
+                padding: '1rem 2rem',
+                height: '500px',
+                overflow: 'auto',
+                borderRadius: '8px',
+              }}
+            >
+              <Title level={5} style={{ marginBottom: '1rem !important' }}>
+                {hasSelected
+                  ? `Completed ${selectedRowKeys.length}/${data.length} of the detail task`
+                  : 'Detail task'}
+              </Title>
+              <Table
+                dataSource={data}
+                columns={columns}
+                rowSelection={{
+                  type: 'checkbox',
+                  ...rowSelection,
+                }}
+                scroll={{ y: 240 }}
+                pagination={false}
+                expandable={{
+                  expandedRowRender: (record) =>
+                    record.attachmentsOfDetailTask.length > 0 ? (
+                      record.attachmentsOfDetailTask.map((attach) => {
+                        return (
+                          <p
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => {
+                              history.push(`/${idProject}/storage`);
+                              localStorage.setItem('sider', '3');
+                            }}
+                          >
+                            {attach.name}
+                          </p>
+                        );
+                      })
+                    ) : (
+                      <Empty description={<span>No attachment</span>} />
+                    ),
+                }}
+                footer={() => (
+                  <Button
+                    type="primary"
+                    size="large"
+                    block
+                    onClick={() => {
+                      setVisible(true);
+                    }}
+                    style={{ borderRadius: '8px' }}
+                  >
+                    Add new one
+                  </Button>
+                )}
               />
-            </Space>
+            </div>
           </Space>
-          <div
-            style={{
-              width: '100%',
-              backgroundColor: 'white',
-              padding: '1rem 2rem',
-              height: '500px',
-              overflow: 'auto',
-              borderRadius: '8px',
-            }}
-          >
-            <Title level={5} style={{ marginBottom: '1rem !important' }}>
-              {hasSelected
-                ? `Completed ${selectedRowKeys.length}/${data.length} of the detail task`
-                : 'Detail task'}
-            </Title>
-            <Table
-              dataSource={testdata}
-              columns={columns}
-              rowSelection={{
-                type: 'checkbox',
-                ...rowSelection,
-              }}
-              scroll={{ y: 240 }}
-              pagination={false}
-              expandable={{
-                expandedRowRender: (record) =>
-                  record.attachments.length > 0 &&
-                  record.attachments.map((attach) => {
-                    return (
-                      <p
-                        style={{ cursor: 'pointer' }}
-                        onClick={() => {
-                          history.push(`/${idProject}/storage`);
-                          localStorage.setItem('sider', '3');
-                        }}
-                      >
-                        {attach.name}
-                      </p>
-                    );
-                  }),
-              }}
-              footer={() => (
-                <Button
-                  type="primary"
-                  size="large"
-                  block
-                  onClick={() => {
-                    setVisible(true);
-                  }}
-                  style={{ borderRadius: '8px' }}
-                >
-                  Add new one
-                </Button>
-              )}
-            />
-          </div>
-        </Space>
-        <ChatOnTask />
-      </div>
+          <ChatOnTask />
+        </div>
+      </Spin>
     </>
   );
 };
