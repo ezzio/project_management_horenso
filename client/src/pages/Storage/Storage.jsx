e737711a7203e402e7dd92407bbd41fe235df7f4
lcnhut070200 <lcnhut070200@gmail.com>
2022-01-17 23:16:13 +0700

    feat: download file in storage page

--- client/src/pages/Storage/Storage.jsx
+++ client/src/pages/Storage/Storage.jsx
@@ -8,7 +8,8 @@ import { RiDownload2Fill } from "react-icons/ri";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useLocation, useParams } from "react-router-dom";
 import "./Storage.scss";
-
+import { AiFillFileZip } from "react-icons/ai";
+import { RiFileExcel2Fill, RiFileWord2Fill } from "react-icons/ri";
 const Storage = () => {
   const dataApi = useSelector((state) => state.storage.dataFile);
 
@@ -71,18 +72,38 @@ const Storage = () => {
     />
   );
 
+  // get icon by file type
+  const renderIconByFileType = (type) => {
+    switch (type) {
+      case "application/zip":
+        return <AiFillFileZip className="icon" />;
+      case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        return <RiFileWord2Fill className="icon" />;
+      case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+        return <RiFileExcel2Fill className="icon" />;
+      default:
+        break;
+    }
+  };
+  // <------------------------------
+
   const columns = [
     {
       title: "Name",
       dataIndex: "name",
       key: "name",
-      render: (t, r) => r.name,
+      render: (t, r) => (
+        <div className="file__name">
+          {renderIconByFileType(r.nameType)}
+          {r.name}
+        </div>
+      ),
     },
     // {
     //   title: "Task",
     //   dataIndex: "task",
     //   key: "Id",
-    //   sorter: (a, b) => a.task.localeCompare(b.task),
+    //   render: (t, r) => r.URL,
     // },
 
     {
@@ -112,7 +133,9 @@ const Storage = () => {
       key: "action",
       render: (text, record) => (
         <Space size="middle">
-          <Button type="primary" icon={<RiDownload2Fill />} size="medium" />
+          <a href={record.URL} download>
+            <Button type="primary" icon={<RiDownload2Fill />} size="medium" />
+          </a>
           <DeleteFile fileName={record.name} />
         </Space>
       ),
