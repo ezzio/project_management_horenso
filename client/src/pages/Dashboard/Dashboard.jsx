2c1d1bf7bc415ad90abac129c2e1a8db07bf8d06
nguyendang127 <nguyendang.ct12@gmail.com>
2022-01-20 19:55:06 +0700

    fix: refactor DashBoard page

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from "react";
 // library dependencies
-import { Space, Typography } from 'antd';
+import { Space, Typography, Tabs } from "antd";
 
 // icon
-import { FundOutlined, AlertOutlined } from '@ant-design/icons';
+import {
+  FundOutlined,
+  AlertOutlined,
+  RocketOutlined,
+  PieChartOutlined,
+} from "@ant-design/icons";
 // css
-import './Dashboard.scss';
+import "./Dashboard.scss";
 
 // components
-import LinePlot from './components/LinePlot/LinePlot';
-import ActivityFeed from './components/ActivityFeed/ActivityFeed';
-import TaskCounter from './components/TaskCounter/TaskCounter';
+import LinePlot from "./components/LinePlot/LinePlot";
+import ActivityFeed from "./components/ActivityFeed/ActivityFeed";
+import TaskCounter from "./components/TaskCounter/TaskCounter";
+import JobStatus from "./components/JobStatus/JobStatus";
+import TargetPercent from "./components/TargetPercent/TargetPercent";
 const { Title } = Typography;
+const { TabPane } = Tabs;
 
 const Dashboard = () => {
   return (
@@ -23,8 +31,8 @@ const Dashboard = () => {
         <div className="dashboard__body__left">
           <div className="left-content">
             <div className="left-content__top">
-              <Space style={{ marginBottom: '1rem' }}>
-                <FundOutlined style={{ fontSize: '1rem' }} />
+              <Space style={{ marginBottom: "1rem" }}>
+                <FundOutlined style={{ fontSize: "1rem" }} />
                 <Title level={5} style={{ marginBottom: 0, lineHeight: 0 }}>
                   Jobs Progress
                 </Title>
@@ -32,17 +40,61 @@ const Dashboard = () => {
               <LinePlot />
             </div>
             <div className="left-content__bottom">
-              <Space style={{ marginBottom: '1rem' }}>
-                <AlertOutlined style={{ fontSize: '1rem' }} />
+              <Space style={{ marginBottom: "1rem" }}>
+                <AlertOutlined style={{ fontSize: "1rem" }} />
                 <Title level={5} style={{ marginBottom: 0, lineHeight: 0 }}>
                   Activity feed
                 </Title>
               </Space>
-              <ActivityFeed />
             </div>
           </div>
         </div>
-        <div className="dashboard__body__right"></div>
+        <div className="dashboard__body__right">
+          <div className="right-content">
+            <div className="right-content__top">
+              <Tabs defaultActiveKey="1" centered>
+                <TabPane
+                  tab={
+                    <span>
+                      <RocketOutlined style={{ fontSize: "1rem" }} />
+                      Target's Percent
+                    </span>
+                  }
+                  key="1"
+                >
+                  <TargetPercent />
+                </TabPane>
+                <TabPane
+                  tab={
+                    <span>
+                      <PieChartOutlined style={{ fontSize: "1rem" }} />
+                      Job's Status
+                    </span>
+                  }
+                  key="2"
+                >
+                  <JobStatus />
+                </TabPane>
+              </Tabs>
+            </div>
+            <div className="right-content__bottom">
+              <div className="right-content__bottom__title">
+                <Space style={{ marginBottom: "1rem" }}>
+                  <AlertOutlined style={{ fontSize: "1rem" }} />
+                  <Title level={5} style={{ marginBottom: 0, lineHeight: 0 }}>
+                    Activity Feed
+                  </Title>
+                </Space>
+              </div>
+              <div className="right-content__bottom__feeds">
+                <ActivityFeed />
+                <ActivityFeed />
+                <ActivityFeed />
+                <ActivityFeed />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/Dashboard/components/ActivityFeed/ActivityFeed.jsx
+++ client/src/pages/Dashboard/components/ActivityFeed/ActivityFeed.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Avatar, Comment, Tooltip, Typography } from "antd";
+import moment from "moment";
+
+const { Title, Text } = Typography;
 
 const ActivityFeed = () => {
-  return <div></div>;
+  return (
+    <Comment
+      // actions={actions}
+      author={<Title level={5}>Han Solo</Title>}
+      avatar={
+        <Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+      }
+      content={<Text>Hello There.</Text>}
+      datetime={
+        <Tooltip title={moment().format("YYYY-MM-DD HH:mm:ss")}>
+          <span>{moment().fromNow()}</span>
+        </Tooltip>
+      }
+    />
+  );
 };
 
 export default ActivityFeed;
