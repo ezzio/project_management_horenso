9d6522753ac65030371a3b1cef744acaf7bb694f
pumk <ddkhoa1206@gmail.com>
2022-01-23 17:32:23 +0700

    fix: set content header fit container below

--- client/src/pages/UserSettings/Component/Header/Header.jsx
+++ client/src/pages/UserSettings/Component/Header/Header.jsx
@@ -9,8 +9,7 @@ import './Header.scss';
 const { Title, Text } = Typography;
 const Header = () => {
   const [notifications, setNotifications] = useState([
-    {title: 'Bạn vừa nhận một task #012 trong dự án #12'},
-
+    { title: 'Bạn vừa nhận một task #012 trong dự án #12' },
   ]);
   const notificationsListRender = (
     <Menu>
@@ -27,25 +26,27 @@ const Header = () => {
 
   return (
     <div className="user-setting-header">
-      <Title level={2}>Kanso</Title>
-      <Space size={'large'}>
-        <Badge dot={Boolean(notifications.length > 0)}>
-          {notifications.length > 0 ? (
-            <Dropdown overlay={notificationsListRender} trigger={['click']}>
-              <BellOutlined
-                style={{ fontSize: 24, color: 'black', cursor: 'pointer' }}
-              />
-            </Dropdown>
-          ) : (
-            <Tooltip title="You don't have any notification" placement="left">
-              <BellOutlined style={{ fontSize: 24, color: 'black' }} />
-            </Tooltip>
-          )}
-        </Badge>
-        <Tooltip title="Log out">
-          <LogoutOutlined style={{ fontSize: 24, cursor: 'pointer' }} />
-        </Tooltip>
-      </Space>
+      <div className="user-setting-header__content">
+        <Title level={2}>Kanso</Title>
+        <Space size={'large'} style={{marginRight: '4%'}}>
+          <Badge dot={Boolean(notifications.length > 0)}>
+            {notifications.length > 0 ? (
+              <Dropdown overlay={notificationsListRender} trigger={['click']}>
+                <BellOutlined
+                  style={{ fontSize: 24, color: 'black', cursor: 'pointer' }}
+                />
+              </Dropdown>
+            ) : (
+              <Tooltip title="You don't have any notification" placement="left">
+                <BellOutlined style={{ fontSize: 24, color: 'black' }} />
+              </Tooltip>
+            )}
+          </Badge>
+          <Tooltip title="Log out">
+            <LogoutOutlined style={{ fontSize: 24, cursor: 'pointer' }} />
+          </Tooltip>
+        </Space>
+      </div>
     </div>
   );
 };
