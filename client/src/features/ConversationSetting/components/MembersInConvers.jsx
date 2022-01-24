7899e750f07d15e53d82ea5fd7f21f5b7c19d6d7
nguyendang127 <nguyendang.ct12@gmail.com>
2022-01-24 22:11:45 +0700

    feat: online status bar at conversation

---
+++ client/src/features/ConversationSetting/components/MembersInConvers.jsx
@@ -0,0 +1,24 @@
+import { Avatar, Badge, Typography } from 'antd';
+import React from 'react';
+
+const { Text } = Typography;
+const MembersInConvers = (props) => {
+  const { user_displayName, avatarUrl, is_online } = props;
+  console.log(props);
+  return (
+    <>
+      <Badge
+        dot='true'
+        status={is_online === true ? 'success' : 'default'}
+        offset={[-5, 5]}
+      >
+        <Avatar size='large' src={avatarUrl} alt='Han Solo' />
+      </Badge>
+      <Text style={{ marginLeft: '1rem' }} className='user-display-name'>
+        {user_displayName}
+      </Text>
+    </>
+  );
+};
+
+export default MembersInConvers;
