b852e7ec1c5a1d3cce952c6bb47d2cb281aedebb
pumk <ddkhoa1206@gmail.com>
2022-01-25 15:27:49 +0700

    chore: delete server folder

--- server/public/db/configmongoose.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-
-let mongose = mongoose.connect("mongodb://localhost:27017/Horenso_Manager", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-module.exports = mongose;
