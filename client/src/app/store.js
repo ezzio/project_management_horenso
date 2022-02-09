9caf96e3dece2c06ea7f60797716437b3c274d70
pumk <ddkhoa1206@gmail.com>
2022-02-09 13:32:19 +0700

    fix: import sign up

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -15,12 +15,12 @@ import chatBoxReducer from 'features/ChatBox/ChatBoxSlice';
 import settingReducer from 'features/Setting/settingSlice';
 import meetingRoomReducer from 'features/MRoom/meetingRoomSlice';
 import dashboardReducer from 'pages/Dashboard/dashboardSlice';
-
+import signUpReducer from 'features/SignUp/signUpSlice';
 export const store = configureStore({
   reducer: {
     board: boardReducer,
     login: loginReducer,
-    signup: signupReducer,
+    signup: signUpReducer,
     timeline: timelineReducer,
     kanban: kanbanReducer,
     userSetting: userSettingReducer,
