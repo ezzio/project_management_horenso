c4bba0d3ba1ad1567cd1be05d5018c23fc756c88
lcnhut070200 <lcnhut070200@gmail.com>
2022-04-11 11:09:10 +0700

    feat: change default role

--- src/features/Teammate/TeammateFeature.jsx
+++ src/features/Teammate/TeammateFeature.jsx
@@ -1,25 +1,25 @@
-import { Input, Space, Spin, Table } from 'antd';
-import 'antd/dist/antd.css';
-import React, { useEffect, useState } from 'react';
-import { BsSearch } from 'react-icons/bs';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import ModalAddTeammate from './components/AddNewTeammate/ModalAddTeammate';
-import DeleteTeammate from './components/DeleteTeammate/DeleteTeammate';
-import EditTeammate from './components/EditTeammate/EditTeammate';
-import './TeammateFeature.scss';
-import { ListUser } from './teammateSlice';
-import { Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import { viewMemberInfo } from 'features/ModalCheckProfileMember/CheckProfileMemberSlice';
-import ModalCheckProfileMember from 'features/ModalCheckProfileMember/ModalCheckProfileMember';
+import { Input, Space, Spin, Table } from "antd";
+import "antd/dist/antd.css";
+import React, { useEffect, useState } from "react";
+import { BsSearch } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import ModalAddTeammate from "./components/AddNewTeammate/ModalAddTeammate";
+import DeleteTeammate from "./components/DeleteTeammate/DeleteTeammate";
+import EditTeammate from "./components/EditTeammate/EditTeammate";
+import "./TeammateFeature.scss";
+import { ListUser } from "./teammateSlice";
+import { Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { viewMemberInfo } from "features/ModalCheckProfileMember/CheckProfileMemberSlice";
+import ModalCheckProfileMember from "features/ModalCheckProfileMember/ModalCheckProfileMember";
 
 const TeammateFeature = () => {
   const teammateData = useSelector((state) => state.teammate.dataList);
   const isProjectOwner = useSelector((state) => state.teammate.isProjectOwner);
   const loading = useSelector((state) => state.teammate.loading);
   const [teammate, setTeammate] = useState(teammateData);
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const dispatch = useDispatch();
   const role = useSelector((state) => state.sidebar.role);
 
@@ -36,7 +36,7 @@ const TeammateFeature = () => {
 
   // View Member's Profile
   const [visible, setVisible] = useState(false);
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState("");
   const viewProfile = (item) => {
     setUserName(item);
     setVisible(true);
@@ -63,13 +63,13 @@ const TeammateFeature = () => {
 
   const columns = [
     {
-      title: 'Name',
-      dataIndex: 'display_name',
-      key: 'user_name',
+      title: "Name",
+      dataIndex: "display_name",
+      key: "user_name",
       render: (t, r) => (
         <div className="user__tag">
           <Avatar
-            style={{ cursor: 'pointer' }}
+            style={{ cursor: "pointer" }}
             className="img"
             size={32}
             icon={<UserOutlined />}
@@ -81,30 +81,32 @@ const TeammateFeature = () => {
       ),
     },
     {
-      title: 'Username',
-      dataIndex: 'user_name',
-      key: 'user_name',
+      title: "Username",
+      dataIndex: "user_name",
+      key: "user_name",
       render: (t, r) => r.user_name,
     },
     {
-      title: 'Tags',
-      dataIndex: 'tag',
-      key: 'user_name',
+      title: "Tags",
+      dataIndex: "tag",
+      key: "user_name",
       render: (t, r) => r.tag,
     },
     {
-      title: 'Action',
+      title: "Action",
       render: (text, record) => (
         <Space size="middle">
-          {isProjectOwner && record.tag !== 'Leader' && role === 'Leader' && (
-            <>
-              <EditTeammate user={record} idProject={idProject} />
-              <DeleteTeammate
-                user_name={record.user_name}
-                idProject={idProject}
-              />
-            </>
-          )}
+          {isProjectOwner &&
+            record.tag !== "Project Manager" &&
+            role === "Project Manager" && (
+              <>
+                <EditTeammate user={record} idProject={idProject} />
+                <DeleteTeammate
+                  user_name={record.user_name}
+                  idProject={idProject}
+                />
+              </>
+            )}
         </Space>
       ),
     },
@@ -115,7 +117,7 @@ const TeammateFeature = () => {
       tip="Loading..."
       size="large"
       spinning={loading}
-      style={{ width: '100%', height: '100%' }}
+      style={{ width: "100%", height: "100%" }}
     >
       {visible && (
         <ModalCheckProfileMember visible={true} setVisible={setVisible} />
@@ -134,7 +136,7 @@ const TeammateFeature = () => {
               <ModalAddTeammate listTeammate={teammate} idProject={idProject} />
             </div>
           ) : (
-            ''
+            ""
           )}
         </div>
         <div className="content">
